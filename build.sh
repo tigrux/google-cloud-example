b86e7fb3d27ff9d596335a1b35f84fd844e4f0ab
Sandino Flores <tigrux@gmail.com>
2024-05-08 19:30:45 -0600

    Install dependencies provided by apt
    Tested on Ubuntu 24.04

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Install some dependencies via apt
+sudo -E apt update && \
+sudo -E apt install \
+  libc-ares-dev \
+  pkg-config \
+  libre2-dev \
+  libssl-dev \
+  zlib1g-dev \
+  libcurl4-openssl-dev
+
 # Install dependencies that cannot be built using cmake's FetchContent.
 ./install-deps.sh
 
